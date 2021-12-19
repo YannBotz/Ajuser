c70424d7463e8b4bb5e30a339f326e62b1473811
Yan Botz! <94920383+YannBotz@users.noreply.github.com>
2021-12-19 17:04:53 +0700

    Update and rename plugins/owner.js to plugin/owner.js

--- plugin/owner.js
+++ plugin/owner.js
@@ -12,10 +12,10 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*AJUSER created by aju*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*YANZSER created by yanz*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n AJUSER created by aju', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n YANZSER created by yanz', MessageType.text);
         }
     }));
 }
@@ -24,16 +24,16 @@ else if (Config.WORKTYPE == 'public') {
 
     Julie.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '917025868709-1634557305@g.us') {
+        if (message.jid === '6281395861695@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*AJUSER created by aju*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*YANZ created by yanz*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n AJUSER created by aju', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n YANZ BOT created by yanz', MessageType.text);
         }
     }));
 }
